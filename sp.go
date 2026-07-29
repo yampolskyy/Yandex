565e4b9cde0f041ba078ea143773a4a5480bfe67
Yampolsky <ayampolskyy@yandex.ru>
2026-07-29 17:30:41 +0300

    tickets

--- sp.go
+++ sp.go
@@ -1,32 +1,75 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"io"
 	"strings"
 	"time"
 )
 
-func QuizRunner(questions, answers []string, answerCh chan string) int {
-	correct := 0
+type Ticket struct {
+	Ticket string
+	User   string
+	Status string
+	Date   time.Time
+}
 
-	for i := range questions {
-		timeout := make(chan bool, 1)
-		go func() {
-			time.Sleep(1 * time.Second)
-			timeout <- true
-		}()
+func GetTasks(ctx context.Context, r io.Reader, w io.Writer, user, status *string, timeout time.Duration) error {
+	var Tickets = []Ticket{}
 
-		select {
-		case <-timeout:
+	ctx, cancel := context.WithTimeout(ctx, timeout)
+	defer cancel()
+
+	scanner := bufio.NewScanner(r)
+
+	for scanner.Scan() {
+		line := strings.TrimSpace(scanner.Text())
+		if line == "" {
 			continue
-		case answer := <-answerCh:
-			userAnswer := strings.TrimSpace(strings.ToLower(answer))
-			correctAnswer := strings.TrimSpace(strings.ToLower(answers[i]))
+		}
 
-			if userAnswer == correctAnswer {
-				correct++
+		var elements = strings.Split(line, "_")
+		if len(elements) < 4 {
+			continue
+		}
+
+		if strings.Contains(elements[0], "TICKET") {
+			if elements[1] != "" {
+				for _, i := range []string{"Готово", "В работе", "Не будет сделано"} {
+					if elements[2] == i {
+						parsedTime, err := time.Parse("2006-01-02", elements[3])
+						if err == nil {
+							if user == nil && status == nil {
+								Tickets = append(Tickets, Ticket{elements[0], elements[1], elements[2], parsedTime})
+
+							} else if user != nil && status == nil {
+								if elements[1] == *user {
+									Tickets = append(Tickets, Ticket{elements[0], elements[1], elements[2], parsedTime})
+								}
+
+							} else if user == nil && status != nil {
+								if elements[2] == *status {
+									Tickets = append(Tickets, Ticket{elements[0], elements[1], elements[2], parsedTime})
+								}
+
+							} else if user != nil && status != nil {
+								if elements[1] == *user && elements[2] == *status {
+									Tickets = append(Tickets, Ticket{elements[0], elements[1], elements[2], parsedTime})
+								}
+							}
+						}
+					}
+				}
 			}
 		}
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
+		}
 	}
-
-	return correct
+	var encoder = json.NewEncoder(w)
+	return encoder.Encode(Tickets)
 }
